bbd728d851223127e3af21ebb7371fe2f31f422a
Vasu Beachoo <vasubeachoo@gmail.com>
2022-04-24 17:25:00 -0400

    Update form validation

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const emailInput = document.querySelector(".email-input");
 const errorMsgBox = document.querySelector(".error-msg-container");
 
 const errorMsg = document.createElement("p");
-errorMsg.innerText = "Please provide a valid email address";
 errorMsg.className = "error-msg red-text regular-text";
 
 function displayErrorMsg() {
@@ -11,8 +10,15 @@ function displayErrorMsg() {
 }
 
 form.addEventListener("submit", (e) => {
-  if (!emailInput.validity.valid) {
+  if (emailInput.value == "") {
+    errorMsg.innerText = "Whoops! It looks like you forgot to add your email";
     e.preventDefault();
     displayErrorMsg();
+  } else {
+    errorMsg.innerText = "Please provide a valid email address";
+    if (!emailInput.validity.valid) {
+      e.preventDefault();
+      displayErrorMsg();
+    }
   }
 });
